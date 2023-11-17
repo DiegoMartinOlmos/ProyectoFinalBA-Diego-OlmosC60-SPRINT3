01cb8763e8abf59047abd9b9083a933e2c26ec84
Diego Olmos <dieguito.olmos14@gmail.com>
2023-11-16 22:34:05 -0300

    añadiendo los archivos

---
+++ scripts/mi-script.js
@@ -0,0 +1,13 @@
+// Funcion que me redirecciona a mi pagina cuando hago click sobre el boton comprar
+
+function redireccion() {
+    window.location.href = "pages/Pagina2.html";
+}
+
+// Funcion que redirecciona cuando le das click sobre el icono
+
+function redireccionFormulario() {
+    window.location.href = "pages/Pagina3.html";
+}
+
+
